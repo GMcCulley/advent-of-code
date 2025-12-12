d9a23f5474d174be2b7e588c4ac8665ae89654c4
Gavin McCulley <91146209+GMcCulley@users.noreply.github.com>
2025-12-11 22:26:10 -0600

    Day 1 Part 2 Complete

---
+++ 2025/Day 1 - Secret Entrance/part2.js
@@ -0,0 +1,24 @@
+let input = require('fs').readFileSync('./input.txt', 'utf-8');
+
+// Parse input into array, removing last empty element
+input = input.replaceAll('\r', '').split('\n');
+
+let zeroSum = 0;
+let num = 50;
+
+for (let item of input) {
+    const direction = item[0];
+    let rotations = item.substring(1);
+
+    while (rotations > 0) {
+        num += direction == 'R' ? 1 : -1;
+        if (num < 0) num = 99;
+        if (num > 99) num = 0;
+        rotations--;
+        if (num == 0) {
+            zeroSum++;
+        }
+    }
+}
+
+console.log(zeroSum);
